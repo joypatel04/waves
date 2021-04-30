a9101888bd7d27d774e5b0a655fba4946849991f
Joy Patel <joy@betterhalf.ai>
2021-04-30 16:46:20 +0530

    Add Splash screen to ios and android

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,8 +1,17 @@
-import {useFocusEffect} from '@react-navigation/native';
-import React, {useState} from 'react';
-import {Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import RNBootSplash from 'react-native-bootsplash';
+import {View} from 'react-native';
 
 const Home = ({count, asyncIncrement, increment, resetCount}) => {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await RNBootSplash.hide({fade: true});
+    });
+  }, []);
   const [loading, setLoading] = useState(true);
 
   return (
